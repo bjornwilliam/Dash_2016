2a877484f486fe14fe1928a07dbb1168c4914b23
bjornwilliam <bjorn.omholt@gmail.com>
2016-04-27 22:27:41 +0200

    add pr0oject

---
+++ Dash_2016/main.c
@@ -0,0 +1,21 @@
+/*
+ * Dash_2016.c
+ *
+ * Created: 4/27/2016 10:26:24 PM
+ * Author : Will
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
